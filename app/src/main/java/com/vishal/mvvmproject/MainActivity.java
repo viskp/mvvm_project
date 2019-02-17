06748bb8e7e623b09a639db162f5c83ce4a6ef59
Vishal Pandey <viskp@live.in>
2019-02-17 18:26:58 +0530

    docs added

--- app/src/main/java/com/vishal/mvvmproject/MainActivity.java
+++ app/src/main/java/com/vishal/mvvmproject/MainActivity.java
@@ -3,6 +3,9 @@ package com.vishal.mvvmproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Main landing activity
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
